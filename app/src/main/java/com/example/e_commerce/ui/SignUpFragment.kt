18b3a817614407be16fdc32725f646123c83d3ba
rmeysaaltay <altayrumeysa9@gmail.com>
2024-10-09 12:19:41 +0300

    created new backgrounds

--- app/src/main/java/com/example/e_commerce/ui/SignUpFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/SignUpFragment.kt
@@ -1,10 +1,12 @@
 package com.example.e_commerce.ui
 
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.e_commerce.databinding.FragmentSignUpBinding
@@ -21,6 +23,7 @@ class SignUpFragment : Fragment() {
     ): View? {
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
